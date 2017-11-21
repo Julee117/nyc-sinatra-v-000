8148b1197e5b19eb348eec9a3baae51666d24476
Julianne Kim <juliannne@gmail.com>
2017-11-21 19:18:40 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -30,6 +30,10 @@ class LandmarksController < ApplicationController
   post '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
     @landmark.update(params[:landmark])
+    OR
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
+    @landmark.save
     redirect "/landmarks/#{@landmark.id}"
   end
 end
