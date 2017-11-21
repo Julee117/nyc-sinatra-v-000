7a197d5eff870a0d4d1fca4bf6b730fe0e1ea15c
Julianne Kim <juliannne@gmail.com>
2017-11-21 19:35:50 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,13 +12,15 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params[:figure])
     if !params[:title][:name].empty?
-      @title = Title.create(params[:title])
-      @figure.title_ids = @title.id
+      # @title = Title.create(params[:title])
+      # @figure.title_ids = @title.id
+      @figure.titles << Title.create(params[:title])
     end
 
     if !params[:landmark][:name].empty?
-      @landmark = Landmark.create(params[:landmark])
-      @figure.landmark_ids = @landmark.id
+      # @landmark = Landmark.create(params[:landmark])
+      # @figure.landmark_ids = @landmark.id
+      @figure.landmarks << Landmark.create(params[:landmark])
     end
 
     redirect "/figures/#{@figure.id}"
